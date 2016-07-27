271b7c3215079ab100c4524ac9187ba8680abeb3
quiquev <luis.enrique.villasenor@gmail.com>
2016-07-27 15:47:33 -0400

    Se agregó el directorio para pruebas.

---
+++ public/qa/render.js
@@ -0,0 +1,9 @@
+var webpage = require('webpage');
+var page = webpage.create();
+
+page.viewportSize = {width: 1920, height: 1080};
+page.open('http://www.google.com', function(status){
+    page.render('googlePrint.png', {format: 'png', quality: '100'});
+
+    phantom.exit();
+});
---
+++ qa/tests-crosspage.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert,
+    path = require('path'),
+    webPage = require('webpage');
+
+var page;
+
+suite('Cross-Page Tests', function(){
+    setup(function(){
+        page = webPage.create(); 
+    });
+    test('requesting a group rate quote from the hood river tour page should populate the referrer field', function(){
+        var referrer = 'http://localhost:3000/tours/hood-river';
+        page.open(referrer, function(){
+            page.render('image.png');
+            assert(true);
+            phantom.exit();
+            /*browser.clickLink('.requestGroupRate', function(){
+                assert(browser.field('referrer').value === referrer);
+                done();
+            });*/
+        });
+    });
+});
