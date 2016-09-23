c175abae4ba85d967e8dd617f5e87e6733455fda
quiquev <luis.enrique.villasenor@gmail.com>
2016-09-23 18:15:53 -0400

    File upload plug-in added to Vacantion Photo page.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt){
             all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
         },
         jshint: {
-            app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+            //app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+            app: ['meadowlark.js', 'lib/**/*.js'],
             qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
         },
         exec: {
--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fortune = require("./lib/fortune.js");
 var weather = require('./lib/getWeatherData.js');
+var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({
@@ -15,9 +17,9 @@ var handlebars = require('express3-handlebars').create({
         }
     }
 });
+app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
@@ -30,6 +32,20 @@ app.use(function(req, res, next){
     res.locals.partials = res.locals.partials || {};
     res.locals.partials.weather = weather.getWeatherData();
     next();
+});
+
+app.use(require('body-parser')());
+
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function (){
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function(){
+            return '/uploads/' + now;
+        }
+    })(req, res, next);
 });
 
 app.get('/', function(req, res){
@@ -59,12 +75,43 @@ app.get('/nursery-rhyme', function(req, res){
     res.render('nursery-rhyme');
 });
 
+app.get('/thank-you', function(req, res){
+    res.render('thank-you');
+});
+
 app.get('/data/nursery-rhyme', function(req, res){
     res.json({
         animal: 'squirrel',
         bodyPart: 'tail',
         adjective: 'bushy',
         noun: 'heck'
+    });
+});
+
+app.get('/newsletter', function(req, res){
+    res.render('newsletter', {csrf: 'CSRF token should go here!'});
+});
+
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json, html') === 'json'){
+        res.send({success: true});
+    }else{
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('Received fields:\n', fields);
+        console.log('Received files:\n', files);
+        res.redirect(303, '/thank-you');
     });
 });
 
